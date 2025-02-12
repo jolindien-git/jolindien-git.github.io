a5cf8d539126559bab01cf74b4a3942dcb8e6ddb
jolindien <johan.peralez@gmail.com>
2025-02-12 10:54:49 +0100

    iris code #1

--- code/3_iris.py
+++ code/3_iris.py
@@ -48,3 +48,19 @@ stats(sepal_width, "sepal width")
 stats(petal_length, "petal length")
 stats(petal_width, "petal width")
 
+
+# matrice de corrélation
+mat = np.stack([sepal_length, sepal_width, petal_length, petal_width, iris_class])
+corr_matrix = np.corrcoef(mat)
+nb = mat.shape[0]
+print(corr_matrix)
+
+fig, ax = plt.subplots()
+im = ax.imshow(corr_matrix, cmap="coolwarm")
+ax.xaxis.set(ticks=np.arange(nb), ticklabels=("sepal length", "sepal width", "petal length", "petal width", "iris class"))
+ax.yaxis.set(ticks=np.arange(nb), ticklabels=("sepal length", "sepal width", "petal length", "petal width", "iris class"))
+cbar = ax.figure.colorbar(im, ax=ax, format='% .2f')
+for i in range(nb):
+    for j in range(nb):
+        ax.text(j, i, "%.2f" % corr_matrix[i, j], ha='center', va='center', color='r')
+plt.show()
